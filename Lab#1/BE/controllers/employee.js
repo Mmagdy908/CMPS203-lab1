6d2fdfcd2f977412eff46776d5ad8584db029db7
Mohamed Magdy <ymmagdyfofo@gmail.com>
2025-02-15 18:56:07 +0200

    lab1

--- Lab#1/BE/controllers/employee.js
+++ Lab#1/BE/controllers/employee.js
@@ -6,15 +6,17 @@ exports.getEmployees = async (req, res, next) => {
 
 // TODO
 exports.deleteEmployee = async (req, res, next) => {
-  employee.splice(
-    employee.findIndex((emp) => emp.id === req.params.id),
-    1
-  );
-  res.status(204).json({ data: null });
+  const index = employee.findIndex((emp) => emp.id === req.params.id);
+
+  if (index === -1)
+    return res.status(404).json({ status: "fail", message: "invalid id" });
+
+  employee.splice(index, 1);
+  res.status(204).json({ status: "succes", data: null });
 };
 
 // TODO
 exports.createEmployee = async (req, res, next) => {
   employee.push(req.body);
-  res.status(201).json({ data: req.body });
+  res.status(201).json({ status: "succes", data: req.body });
 };
